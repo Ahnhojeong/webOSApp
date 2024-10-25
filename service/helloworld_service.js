a447576d1a8d50c18d8924ee363cfeecc02ab27f
ahnhojeong <spdlqj427@gmail.com>
2024-10-25 17:58:16 +0900

    ✨ Eyemini actCode

--- service/helloworld_service.js
+++ service/helloworld_service.js
@@ -384,6 +384,7 @@ const {
   RequestPacket,
   RequestHeader,
   RequestParamCR2Init,
+  cr_crc32
 } = require('./EyeMiniSdk');
 
 let tcpClientRequest = null;
@@ -659,19 +660,40 @@ service.register('tcpClient/getNotifyPacket', function (message) {
   }
 });
 
-service.register('tcpClientRequest/sendPacketActCode', function (message) {
+service.register('tcpClient/sendPacketActCode', function (message) {
   if (tcpClientRequest == null || tcpClientRequest.isRunning === false) {
     message.respond({
       returnValue: false,
       data: 'tcpClientRequest : invalid',
     });
   } else {
-    const packet = createPacket_GetActCode();
+    const packet = createPacketGetActCode();
+    const req_id = packet.header.req_id;
     tcpClientRequest.sendPacket(packet);
-    message.respond({
-      returnValue: true,
-      data: 'tcpClientRequest : sendPacketActCode',
-    });
+
+    // 응답 대기 (3초 타임아웃 내에 0.5초마다 확인)
+    let checkInterval = setInterval(() => {
+      const ackPacket = tcpClientRequest.receivedPackets.find(
+        (p) => p.header.ack_id === req_id,
+      );
+      if (ackPacket) {
+        clearInterval(checkInterval);
+        clearTimeout(timeoutHandle);
+        message.respond({
+          returnValue: true,
+          data: JSON.stringify(ackPacket),
+        });
+      }
+    }, 500);
+
+    // 타임아웃 설정 (3초 후)
+    let timeoutHandle = setTimeout(() => {
+      clearInterval(checkInterval);
+      message.respond({
+        returnValue: false,
+        data: 'tcpClientRequest : timeout',
+      });
+    }, 3000);
   }
 });
 
@@ -696,6 +718,57 @@ service.register('tcpClientNotify/getLog', function (message) {
   });
 });
 
+let messageGetSensorStatus = null;
+let intervalGetSensorStatus = null;
+service.register('tcpClient/startGetSensorStatus', function (message) {
+  if (tcpClientRequest == null || tcpClientRequest.isRunning === false) {
+    message.respond({
+      returnValue: false,
+      data: 'tcpClientRequest : invalid',
+    });
+  } else {
+    messageGetSensorStatus = message;
+
+    // 일정 주기로 센서 상태 확인 명령 전송
+    if(intervalGetSensorStatus==null){
+      intervalGetSensorStatus = setInterval(function () {
+        if (tcpClientRequest != null && tcpClientRequest.isRunning === true) {
+          {
+            const param = null;
+            const header = new RequestHeader(EyeMiniSdk.JSON_EVENT_CR2CMD, EyeMiniSdk.CR2CMD_SENSORSTATUS, 0);
+            const packet = new RequestPacket(header, param);
+            tcpClientRequest.sendPacket(packet);    
+          }
+          {
+            const param = null;
+            const header = new RequestHeader(EyeMiniSdk.JSON_EVENT_CR2CMD, EyeMiniSdk.CR2CMD_BALLPOSITION, 0);
+            const packet = new RequestPacket(header, param);
+            tcpClientRequest.sendPacket(packet);
+          }
+        }
+      }, 100);
+    }
+
+    message.respond({
+      returnValue: true,
+      data: 'tcpClient : startGetSensorStatus',
+    });
+  }
+});
+service.register('tcpClient/stopGetSensorStatus', function (message) {
+  clearInterval(intervalGetSensorStatus);
+
+  messageGetSensorStatus = null;
+  intervalGetSensorStatus = null;
+
+  message.respond({
+    returnValue: true,
+    data: 'tcpClient : stopGetSensorStatus',
+  });
+
+});
+
+
 class TcpClient {
   constructor(name) {
     this.tcpClient = null;
@@ -707,9 +780,16 @@ class TcpClient {
     this.ip = '';
     this.isRunning = false;
     this.receivedPackets = [];
+    this.logMsgCnt = 0;
   }
   logMessage(message) {
+    if(this.logMsgCnt>=30){
+      this.logMsgCnt = 0;
+      this.tcpLog = "";
+    }
+
     this.tcpLog += message + '\n';
+    this.logMsgCnt++;
   }
   getLog() {
     return this.tcpLog;
@@ -749,7 +829,6 @@ class TcpClient {
       if (packet.header.command !== EyeMiniSdk.CR2CMD_SECURE_GET_ACT_CODE) {
         // 패킷을 문자열로 변환
         this.senStringBuilder = JSON.stringify(packet);
-
         let length = Buffer.byteLength(this.senStringBuilder, 'utf8');
         let packetLength = Buffer.alloc(4); // 4바이트 크기 버퍼
 
@@ -763,20 +842,14 @@ class TcpClient {
         ]);
 
         this.stream.write(packetMessage, () => {
-          this.logMessage('Packet sent : ' + this.senStringBuilder);
+          //this.logMessage('Packet sent : ' + this.senStringBuilder);
         });
       } else {
-        let data = packet.param;
-        let payload = '[' + data.encryptedData0.payload.join(',') + ']';
+        let payload = '[' + packet.param.encryptedData0.payload.join(',') + ']';
+        packet.param.encryptedData0.payload = JSON.parse(payload);
+        let convertJsonString = JSON.stringify(packet);
 
-        this.senStringBuilder = JSON.stringify(packet);
-
-        let jsonObject = JSON.parse(this.senStringBuilder);
-        jsonObject.param.encryptedData0.payload = payload;
-
-        let convertJsonString = JSON.stringify(jsonObject);
         let packetLength = Buffer.alloc(4);
-
         packetLength.writeUInt32BE(convertJsonString.length, 0);
 
         let packetArray = Buffer.from(convertJsonString, 'utf8');
@@ -787,7 +860,7 @@ class TcpClient {
         ]);
 
         this.stream.write(packetMessage, () => {
-          this.logMessage('Security Packet sent ' + convertJsonString);
+          //this.logMessage('Security Packet sent ' + convertJsonString);
         });
       }
     } catch (e) {
@@ -817,21 +890,49 @@ class TcpClient {
         var resultJson = this.extractJson(data);
         if (resultJson != null) {
           resultJson.tcp_client_received = Date.now();
-          this.receivedPackets.push(resultJson);
-
-          if (this.name === 'notify') {
-            wss.clients.forEach((client) => {
-              if (client.readyState === WebSocket.OPEN) {
-                try {
-                  client.send(JSON.stringify(resultJson));
-                } catch (error) {
-                  this.receivedPackets.push(
-                    'Failed to send message to client:',
-                    error,
-                  );
-                }
+          if(this.name === 'request'){
+            if(resultJson.header.command === EyeMiniSdk.CR2CMD_SENSORSTATUS || resultJson.header.command === EyeMiniSdk.CR2CMD_BALLPOSITION){
+              // 센서 상태 확인 응답일 경우
+              if(wss){ 
+                wss.clients.forEach((client) => {
+                  if (client.readyState === WebSocket.OPEN) {
+                    try {
+                      client.send(JSON.stringify(resultJson));
+                    } catch (error) {
+                      this.logMessage('Failed to send message to client:' + error);
+                    }
+                  }
+                });
               }
-            });
+              /*
+              // subscribe 사용 방식
+              if(messageGetSensorStatus!=null){
+                messageGetSensorStatus.respond({
+                  returnValue: true,
+                  data: JSON.stringify(resultJson),
+                });
+              }
+              */
+            } else {
+              // 기타 명령에 대한 응답일 경우
+              this.receivedPackets.push(resultJson);
+            }
+          } else if (this.name === 'notify') {
+            if(wss){ 
+              wss.clients.forEach((client) => {
+                if (client.readyState === WebSocket.OPEN) {
+                  try {
+                    client.send(JSON.stringify(resultJson));
+                  } catch (error) {
+                    this.receivedPackets.push(
+                      'Failed to send message to client:',
+                      error,
+                    );
+                  }
+                }
+              });
+            }
+            this.receivedPackets.push(resultJson);
           }
         }
       });
@@ -870,7 +971,7 @@ class TcpClient {
     }
 
     const jsonString = data.toString().substring(jsonStart);
-    this.logMessage('jsonString : ' + jsonString);
+    //this.logMessage('jsonString : ' + jsonString);
 
     // 추출된 문자열을 JSON으로 파싱
     try {
@@ -943,7 +1044,7 @@ function createPacketCr2CmdSetWifi(encryption, ssid, password, type) {
 }
 
 // CreatePacket_GetActCode 함수 정의
-function createPacket_GetActCode() {
+function createPacketGetActCode() {
   const param = new EncryptedData0();
   param.encryptedData0 = GetActCodeParam();
 
@@ -953,7 +1054,6 @@ function createPacket_GetActCode() {
     );
     return;
   }
-
   const size = param.encryptedData0.payload_length + 4 + 4; // sizeof(uint)는 4바이트로 간주
 
   const header = new RequestHeader(
@@ -961,140 +1061,15 @@ function createPacket_GetActCode() {
     EyeMiniSdk.CR2CMD_SECURE_GET_ACT_CODE,
     size,
   );
+  
   const packet = new RequestPacket(header, param);
+
+  //tcpClientRequest.logMessage(JSON.stringify(packet));
 
   //_sendPacketQueue.enqueue(packet);
   return packet;
 }
 
-// CRC32 Table (same as in C# code)
-const crc_table = [
-  0x00000000, 0x77073096, 0xee0e612c, 0x990951ba, 0x076dc419, 0x706af48f,
-  0xe963a535, 0x9e6495a3, 0x0edb8832, 0x79dcb8a4, 0xe0d5e91e, 0x97d2d988,
-  0x09b64c2b, 0x7eb17cbd, 0xe7b82d07, 0x90bf1d91, 0x1db71064, 0x6ab020f2,
-  0xf3b97148, 0x84be41de, 0x1adad47d, 0x6ddde4eb, 0xf4d4b551, 0x83d385c7,
-  0x136c9856, 0x646ba8c0, 0xfd62f97a, 0x8a65c9ec, 0x14015c4f, 0x63066cd9,
-  0xfa0f3d63, 0x8d080df5, 0x3b6e20c8, 0x4c69105e, 0xd56041e4, 0xa2677172,
-  0x3c03e4d1, 0x4b04d447, 0xd20d85fd, 0xa50ab56b, 0x35b5a8fa, 0x42b2986c,
-  0xdbbbc9d6, 0xacbcf940, 0x32d86ce3, 0x45df5c75, 0xdcd60dcf, 0xabd13d59,
-  0x26d930ac, 0x51de003a, 0xc8d75180, 0xbfd06116, 0x21b4f4b5, 0x56b3c423,
-  0xcfba9599, 0xb8bda50f, 0x2802b89e, 0x5f058808, 0xc60cd9b2, 0xb10be924,
-  0x2f6f7c87, 0x58684c11, 0xc1611dab, 0xb6662d3d, 0x76dc4190, 0x01db7106,
-  0x98d220bc, 0xefd5102a, 0x71b18589, 0x06b6b51f, 0x9fbfe4a5, 0xe8b8d433,
-  0x7807c9a2, 0x0f00f934, 0x9609a88e, 0xe10e9818, 0x7f6a0dbb, 0x086d3d2d,
-  0x91646c97, 0xe6635c01, 0x6b6b51f4, 0x1c6c6162, 0x856530d8, 0xf262004e,
-  0x6c0695ed, 0x1b01a57b, 0x8208f4c1, 0xf50fc457, 0x65b0d9c6, 0x12b7e950,
-  0x8bbeb8ea, 0xfcb9887c, 0x62dd1ddf, 0x15da2d49, 0x8cd37cf3, 0xfbd44c65,
-  0x4db26158, 0x3ab551ce, 0xa3bc0074, 0xd4bb30e2, 0x4adfa541, 0x3dd895d7,
-  0xa4d1c46d, 0xd3d6f4fb, 0x4369e96a, 0x346ed9fc, 0xad678846, 0xda60b8d0,
-  0x44042d73, 0x33031de5, 0xaa0a4c5f, 0xdd0d7cc9, 0x5005713c, 0x270241aa,
-  0xbe0b1010, 0xc90c2086, 0x5768b525, 0x206f85b3, 0xb966d409, 0xce61e49f,
-  0x5edef90e, 0x29d9c998, 0xb0d09822, 0xc7d7a8b4, 0x59b33d17, 0x2eb40d81,
-  0xb7bd5c3b, 0xc0ba6cad, 0xedb88320, 0x9abfb3b6, 0x03b6e20c, 0x74b1d29a,
-  0xead54739, 0x9dd277af, 0x04db2615, 0x73dc1683, 0xe3630b12, 0x94643b84,
-  0x0d6d6a3e, 0x7a6a5aa8, 0xe40ecf0b, 0x9309ff9d, 0x0a00ae27, 0x7d079eb1,
-  0xf00f9344, 0x8708a3d2, 0x1e01f268, 0x6906c2fe, 0xf762575d, 0x806567cb,
-  0x196c3671, 0x6e6b06e7, 0xfed41b76, 0x89d32be0, 0x10da7a5a, 0x67dd4acc,
-  0xf9b9df6f, 0x8ebeeff9, 0x17b7be43, 0x60b08ed5, 0xd6d6a3e8, 0xa1d1937e,
-  0x38d8c2c4, 0x4fdff252, 0xd1bb67f1, 0xa6bc5767, 0x3fb506dd, 0x48b2364b,
-  0xd80d2bda, 0xaf0a1b4c, 0x36034af6, 0x41047a60, 0xdf60efc3, 0xa867df55,
-  0x316e8eef, 0x4669be79, 0xcb61b38c, 0xbc66831a, 0x256fd2a0, 0x5268e236,
-  0xcc0c7795, 0xbb0b4703, 0x220216b9, 0x5505262f, 0xc5ba3bbe, 0xb2bd0b28,
-  0x2bb45a92, 0x5cb36a04, 0xc2d7ffa7, 0xb5d0cf31, 0x2cd99e8b, 0x5bdeae1d,
-  0x9b64c2b0, 0xec63f226, 0x756aa39c, 0x026d930a, 0x9c0906a9, 0xeb0e363f,
-  0x72076785, 0x05005713, 0x95bf4a82, 0xe2b87a14, 0x7bb12bae, 0x0cb61b38,
-  0x92d28e9b, 0xe5d5be0d, 0x7cdcefb7, 0x0bdbdf21, 0x86d3d2d4, 0xf1d4e242,
-  0x68ddb3f8, 0x1fda836e, 0x81be16cd, 0xf6b9265b, 0x6fb077e1, 0x18b74777,
-  0x88085ae6, 0xff0f6a70, 0x66063bca, 0x11010b5c, 0x8f659eff, 0xf862ae69,
-  0x616bffd3, 0x166ccf45, 0xa00ae278, 0xd70dd2ee, 0x4e048354, 0x3903b3c2,
-  0xa7672661, 0xd06016f7, 0x4969474d, 0x3e6e77db, 0xaed16a4a, 0xd9d65adc,
-  0x40df0b66, 0x37d83bf0, 0xa9bcae53, 0xdebb9ec5, 0x47b2cf7f, 0x30b5ffe9,
-  0xbdbdf21c, 0xcabac28a, 0x53b39330, 0x24b4a3a6, 0xbad03605, 0xcdd70693,
-  0x54de5729, 0x23d967bf, 0xb3667a2e, 0xc4614ab8, 0x5d681b02, 0x2a6f2b94,
-  0xb40bbe37, 0xc30c8ea1, 0x5a05df1b, 0x2d02ef8d,
-];
-
-function cr_crc32(crc, buf, len) {
-  if (!buf) return 0;
-
-  crc = (crc ^ 0xffffffff) >>> 0;
-
-  let index = 0;
-  while (len >= 8) {
-    crc = DO8(buf, index, crc);
-    index += 8;
-    len -= 8;
-  }
-  if (len !== 0) {
-    do {
-      crc = DO1(buf, index, crc);
-      index += 1;
-    } while (--len !== 0);
-  }
-  return (crc ^ 0xffffffff) >>> 0;
-}
-
-function DO1(buf, index, crc) {
-  crc = crc_table[(crc ^ buf[index]) & 0xff] ^ (crc >>> 8);
-  return crc >>> 0;
-}
-
-function DO2(buf, index, crc) {
-  crc = DO1(buf, index, crc);
-  crc = DO1(buf, index + 1, crc);
-  return crc >>> 0;
-}
-
-function DO4(buf, index, crc) {
-  crc = DO2(buf, index, crc);
-  crc = DO2(buf, index + 2, crc);
-  return crc >>> 0;
-}
-
-function DO8(buf, index, crc) {
-  crc = DO4(buf, index, crc);
-  crc = DO4(buf, index + 4, crc);
-  return crc >>> 0;
-}
-
-/*
-function cr_crc32(crc, buf, len) {
-  if (!buf) return 0;
-
-  crc ^= 0xffffffff;
-  let i = 0;
-  while (len >= 8) {
-      DO8(buf, () => i++, (val) => { crc = val; }, crc);
-      len -= 8;
-  }
-  if (len !== 0) {
-      do {
-          DO1(buf, () => i++, (val) => { crc = val; }, crc);
-      } while (--len !== 0);
-  }
-  return crc ^ 0xffffffff;
-}
-
-function DO1(buf, getIndex, setCrc, crc) {
-  crc = crc_table[(crc ^ buf[getIndex()]) & 0xff] ^ (crc >>> 8);
-  setCrc(crc);
-}
-
-function DO2(buf, getIndex, setCrc, crc) {
-  DO1(buf, getIndex, setCrc, crc);
-  DO1(buf, getIndex, setCrc, crc);
-}
-
-function DO4(buf, getIndex, setCrc, crc) {
-  DO2(buf, getIndex, setCrc, crc);
-  DO2(buf, getIndex, setCrc, crc);
-}
-
-function DO8(buf, getIndex, setCrc, crc) {
-  DO4(buf, getIndex, setCrc, crc);
-  DO4(buf, getIndex, setCrc, crc);
-}
-*/
 function GetActCodeParam() {
   try {
     // Encryption key and IV (16 bytes each)
@@ -1107,8 +1082,6 @@ function GetActCodeParam() {
       0x00, 0x00, 0x00, 0x00,
     ]);
 
-    //tcpClient.logMessage("GetActCodeParam(0)");
-
     // Create clientInfoBuffer of 64 bytes
     const clientInfoBuffer = Buffer.alloc(64);
 
@@ -1116,9 +1089,6 @@ function GetActCodeParam() {
     for (let i = 0; i < 32; i++) {
       clientInfoBuffer[i] = i;
     }
-
-    // Client_Type: byte
-    clientInfoBuffer[32] = EyeMiniSdk.CLIENT_DEV_TYPE_TABLET;
 
     // Account_String: char[17], set to 0
     for (let i = 0; i < 17; i++) {
@@ -1130,37 +1100,22 @@ function GetActCodeParam() {
       clientInfoBuffer[50 + i] = 0;
     }
 
-    //tcpClient.logMessage("GetActCodeParam(1)");
-
     // Crc: uint32, set to 0 initially
     clientInfoBuffer.writeUInt32LE(0, 60);
+    clientInfoBuffer[49] = EyeMiniSdk.CLIENT_DEV_TYPE_TABLET;
 
     // Calculate CRC32 over clientInfoBuffer excluding Crc field
     const crc = cr_crc32(0, clientInfoBuffer.slice(0, 60), 60) >>> 0;
-
-    tcpClientRequest.logMessage('GetActCodeParam(2) ' + crc);
-
     // Update Crc field
     clientInfoBuffer.writeUInt32LE(crc, 60);
-
-    //tcpClient.logMessage("GetActCodeParam(2-1)");
 
     // Encrypt clientInfoBuffer using AES-128-CBC
     const cipher = crypto.createCipheriv('aes-128-cbc', Compk2, iv2);
 
-    //tcpClient.logMessage("GetActCodeParam(2-2)");
-
     cipher.setAutoPadding(false);
 
-    //tcpClient.logMessage("GetActCodeParam(2-3)");
-
     let encrypted = cipher.update(clientInfoBuffer);
-
-    //tcpClient.logMessage("GetActCodeParam(2-4)");
-
     encrypted = Buffer.concat([encrypted, cipher.final()]);
-
-    //tcpClient.logMessage("GetActCodeParam(3)");
 
     // Create EncryptedData buffer
     const payloadLength = encrypted.length;
@@ -1175,8 +1130,6 @@ function GetActCodeParam() {
     // Copy encrypted payload
     encrypted.copy(encryptedDataBuffer, 8);
 
-    //tcpClient.logMessage("GetActCodeParam(4)");
-
     // Calculate CRC over EncryptedData excluding Crc field
     const crcEncrypted = cr_crc32(
       0,
@@ -1187,16 +1140,14 @@ function GetActCodeParam() {
       4 + payloadLength,
     );
 
-    tcpClientRequest.logMessage('GetActCodeParam(5)');
-
     // Update Crc field
     encryptedDataBuffer.writeUInt32LE(crcEncrypted, 4);
 
     // Return EncryptedData object
     return {
       payload_length: payloadLength,
-      crc: crcEncrypted,
       payload: encrypted,
+      crc: crcEncrypted,
     };
   } catch (e) {
     tcpClientRequest.logMessage('GetActCodeParam Error - ' + e.message);
@@ -1295,139 +1246,9 @@ function parseActCode(buffer) {
   };
 }
 
-// AckPacket 클래스 정의
-class AckPacket {
-  constructor(header, param) {
-    this.Header = header;
-    this.Param = param;
-  }
-}
-
-// AckHeader 클래스 정의
-class AckHeader {
-  constructor(status, answer, command, ackId, length) {
-    this.status = status;
-    this.answer = answer;
-    this.command = command;
-    this.ack_id = ackId;
-    this.length = length;
-  }
-}
-
-// 패킷 관련 데이터 구조 정의
-
-// RequestParam_Set_RightLeft 구조체 정의
-class RequestParam_Set_RightLeft {
-  constructor(rightLeft) {
-    this.rightLeft = rightLeft;
-  }
-}
-
-// RequestParam_Set_Wifi 구조체 정의
-class RequestParam_Set_Wifi {
-  constructor(configuration) {
-    this.configuration = configuration;
-  }
-}
-
-// RequestParam_Set_Wifi_Data 구조체 정의
-class RequestParam_Set_Wifi_Data {
-  constructor(encryption, ssid, password, type) {
-    this.encryption = encryption;
-    this.ssid = ssid;
-    this.password = password;
-    this.type = type;
-  }
-}
-
-// AckParam_BallPosition 구조체 정의
-class AckParam_BallPosition {
-  constructor(teeBallPos, ironBallPos, puttingBallPos) {
-    this.teeBallPos = teeBallPos;
-    this.ironBallPos = ironBallPos;
-    this.puttingBallPos = puttingBallPos;
-  }
-}
-
-// AckParam_BallPosition_Result 구조체 정의
-class AckParam_BallPosition_Result {
-  constructor(ballExist, shotResult, x, y, z) {
-    this.ballexist = ballExist;
-    this.shotresult = shotResult;
-    this.x = x;
-    this.y = y;
-    this.z = z;
-  }
-}
-
-// AckParam_Get_wifi 구조체 정의
-class AckParam_Get_wifi {
-  constructor(status, wifiInfo) {
-    this.status = status;
-    this.wifiInfo = wifiInfo;
-  }
-}
-
-// AckParam_Get_WifiInfo 구조체 정의
-class AckParam_Get_WifiInfo {
-  constructor(ssid, quality, level) {
-    this.ssid = ssid;
-    this.quality = quality;
-    this.level = level;
-  }
-}
-
-// AckParam_Get_WifiList 구조체 정의
-class AckParam_Get_WifiList {
-  constructor(number, wifiList) {
-    this.number = number;
-    this.wifiList = wifiList;
-  }
-}
-
-// AckParam_WifiList_Data 구조체 정의
-class AckParam_WifiList_Data {
-  constructor(ssidArray, qualityArray, levelArray, encryptionArray) {
-    this.ssidArray = ssidArray;
-    this.qualityArray = qualityArray;
-    this.levelArray = levelArray;
-    this.encryptionArray = encryptionArray;
-  }
-}
-
 // EncryptedData0 클래스 정의
 class EncryptedData0 {
   constructor(encryptedData0) {
     this.encryptedData0 = encryptedData0; // EncryptedData 인스턴스
-  }
-}
-
-// EncryptedData 클래스 정의
-class EncryptedData {
-  constructor(payload_length, payload, crc) {
-    this.payload_length = payload_length; // Number
-    this.payload = payload; // Buffer 또는 Uint8Array (길이 64)
-    this.crc = crc; // Number
-  }
-}
-
-// ClientInfo 클래스 정의
-class ClientInfo {
-  constructor(client_code, account_string, client_type, reserved, crc) {
-    this.client_code = client_code; // Buffer 또는 Uint8Array (길이 32)
-    this.account_string = account_string; // String (길이 17)
-    this.client_type = client_type; // Number (byte)
-    this.reserved = reserved; // Buffer 또는 Uint8Array (길이 10)
-    this.crc = crc; // Number
-  }
-}
-
-// ActCode 클래스 정의
-class ActCode {
-  constructor(success, act_code, reserved, crc) {
-    this.success = success; // Number (int)
-    this.act_code = act_code; // Buffer 또는 Uint8Array (길이 32)
-    this.reserved = reserved; // Buffer 또는 Uint8Array (길이 8)
-    this.crc = crc; // Number
   }
 }
